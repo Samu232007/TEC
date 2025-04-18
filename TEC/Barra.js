69ae90cbdabad6ac683b28433f97eb76af8aa4a6
Samu232007 <samu232007@gmail.com>
2025-04-18 16:08:24 -0600

    Delete TEC directory

--- TEC/Barra.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const sidebar = document.querySelector('.sidebar');
-
-menuToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('active'); // Alterna la clase 'active' en la barra lateral
-});
